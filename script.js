a5a6c245e72e08037ddad87c867a39d1e2e9e68b
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:05:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-//your JS code here. If required.
-//your JS code here. If required.
 const images = [
-    { url: 'https://picsum.photos/id/1/200/300' },
-    { url: 'https://picsum.photos/id/2/200/300' },
-    { url: 'https://picsum.photos/id/3/200/300' },
-    { url: 'https://picsum.photos/id/4/200/300' },
-    { url: 'https://picsum.photos/id/5/200/300' },
-    { url: 'https://picsum.photos/id/6/200/300' },
-    { url: 'https://picsum.photos/id/7/200/300' },
-    { url: 'https://picsum.photos/id/8/200/300' },
-    { url: 'https://picsum.photos/id/9/200/300' },
-    { url: 'https://picsum.photos/id/10/200/300' },
-  ];
-  
-  function downloadImages() {
-    const promises = images.map(image => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-        img.src = image.url;
-      });
-    });
-  
-    Promise.all(promises)
-      .then(images => {
-        const outputDiv = document.getElementById('output');
-        images.forEach(image => {
-          outputDiv.appendChild(image);
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  {
+    url: "https://picsum.photos/id/237/200/300",
+    alt: "Image 1",
+  },
+  {
+    url: "https://picsum.photos/id/238/200/300",
+    alt: "Image 2",
+  },
+  {
+    url: "https://picsum.photos/id/239/200/300",
+    alt: "Image 3",
   }
-  
-  const downloadButton = document.getElementById('download-images-button');
-  downloadButton.addEventListener('click', downloadImages);
-  
+
+];
+
+function downloadImages(images) {
+  const promises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.alt = image.alt;
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+    });
+  });
+
+  Promise.all(promises)
+    .then(imgs => {
+      const output = document.getElementById('output');
+		output.innerHTML = null;
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+const button = document.getElementById('download-images-button');
+button.addEventListener('click', () => {
+  downloadImages(images);
+});
